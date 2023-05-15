43707bc5d2fb2e0e72f0fa8c42fa1d327c443672
Mishel Tyurkova <misheltyurkova@gmail.com>
2023-05-15 14:36:23 +1000

    commit tree c9c62a1eb06b3a2ec35df632eec956bcc8cb958c

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    return customerName.toUpperCase();
+}
+
+upperCaseCustomerName()
+
+var bestCustomer; 
+
+function setBestCustomer() {
+    return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'rob'
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'bob'
+}
